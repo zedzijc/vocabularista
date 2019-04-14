199a0551dd464de666394962eee28b79d11c3fea
ZEDZIJC <anders.axner@live.se>
2019-04-15 01:02:28 +0200

    Add object support for translations
    A simple class that represents a translation. Could have been stored as a blob in the database, but it feels cleaner to not work with blobs.

---
+++ src/language.py
@@ -0,0 +1,22 @@
+
+class Translation(object):
+
+    def __init__(self, foreign_word, native_word, score=0):
+        self.foreign_word = foreign_word
+        self.native_word = native_word
+        self.score = score
+
+    def get_foreign_word(self):
+        return self.foreign_word
+
+    def get_native_word(self):
+        return self.native_word
+
+    def get_score(self):
+        return self.score
+
+    def update_score(self, score_delta):
+        if self.score + score_delta < 0:
+            self.score = 0
+        else:
+            self.score += score_delta
